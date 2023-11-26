e03efa493db21067717f676a05aa0ce54c23f8e0
Jesus Ramirez <105754626+Traquinero@users.noreply.github.com>
2023-11-26 14:35:12 -0600

    Add files via upload

---
+++ Promedio de tres numeros.py
@@ -0,0 +1,6 @@
+num1 = float(input("Dame el num1: "))
+num2 = float(input("Dame el num2: "))
+num3 = float(input("Dame el num3: "))
+prom = float((num1 + num2 + num3) / 3)
+print("Promedio: ", prom)
+
