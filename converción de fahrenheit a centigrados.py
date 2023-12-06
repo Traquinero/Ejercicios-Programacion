4d299050e6ae49b11e83b0b610e8ee3c40a3f070
Jesus Ramirez <105754626+Traquinero@users.noreply.github.com>
2023-12-06 17:03:27 -0600

    Add files via upload

---
+++ converción de fahrenheit a centigrados.py
@@ -0,0 +1,8 @@
+# Calcular la converción de fahrenheit a centigrados
+# Para calcular los centigrados se debe restar los grados fahrenheit - 32 y dividir el resultado entre 1.8
+
+
+fahren = float(input("Dame los grados fahrenheit: "))
+conv = (fahren -32) / 1.8
+print(conv, "Grados Celsius")
+
