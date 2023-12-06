11b8abfffb2504d8f1d871a104589a8121297fdf
Jesus Ramirez <105754626+Traquinero@users.noreply.github.com>
2023-12-05 21:11:23 -0600

    Add files via upload

---
+++ Ejemplo de contadores.py
@@ -0,0 +1,19 @@
+# Contador positivo
+cont = 0
+while cont < 15:
+    print("Contador en uno a uno: ", cont)
+    cont = cont + 1
+cont2 = 0
+while cont2 < 8:
+    print("Contador de dos en dos: ", cont2)
+    cont2 = cont2 + 2
+
+# contador negativo
+cont3 = 0
+while cont3 > -10:
+    print("Contador de menos a menos uno: ", cont3)
+    cont3 = cont3 - 1
+cont4 = 0
+while cont4 > -6:
+    print("Contador de menos a menos dos: ", cont4)
+    cont4 = cont4 - 2
